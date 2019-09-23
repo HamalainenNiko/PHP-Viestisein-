6e2dcf059c835b80ad20d4ea1ae3b5af861416ad
Niko Hämäläinen <niko.hamalainen@itvelhot.local>
2019-09-23 12:52:22 +0300

    ok retards

---
+++ GFHLAAAA/multi_login/functions.php
@@ -0,0 +1,86 @@
+<?php 
+session_start();
+
+$db = mysqli_connect('localhost','root','','multi_login');
+
+$username = "";
+$email = "";
+$errors = array();
+
+if(isset($_POST['register_btn'])){
+    register();
+}
+
+function register(){
+    global $db, $errors, $username, $email;
+
+    $username = e($_POST['username']);
+    $email = e($_POST['email']);
+    $password_1 = e($_POST['password_1']);
+    $password_2 = e($_POST['password_2']);
+
+    if(empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if(empty($email)){
+        array_push($errors, "Email is required");
+    }
+    if(empty($password_1)){
+        array_push($errors, "Password is required");
+    }
+    if($password_1 != $password_2){
+        array_push($errors, "Given passwords don't match");
+    }
+
+    if (count($errors) == 0) {
+        $password = md5($password_1); //Encrypt password before saving
+
+        if(isset($_POST['user_type'])) {
+            $user_type = e($_POST['user_type']);
+            $query = "INSERT INTO users (username, email, user_type, password) 
+					  VALUES('$username', '$email', '$user_type', '$password')";
+            mysqli_query($db, $query);
+            $_SESSION['success'] = "New user created";
+            header('location: home.php');
+        }else{
+            $query = "INSERT INTO users (username, email, user_type, password)
+            VALUES('$username', '$email', 'user', '$password')";
+            mysqli_query($db, $query);
+            
+            $logged_in_user_id = mysqli_insert_id($db);
+
+            $_SESSION['user'] = getUserById($logged_in_user_id);
+            $_SESSION['success'] = "logged in";
+            header('location: index.php');
+        }
+    }
+}
+
+function getUserById($id){
+    global $db;
+    $query = "SELECT * FROM users WHERE id=" . $id;
+    $result = mysqli_query($db, $query);
+
+    $user = mysqli_fetch_assoc($result);
+    return $user;
+}
+
+function e($val){
+    global $db;
+    return mysqli_real_escape_string($db, trim($val));
+}
+
+function display_error(){
+    global $errors;
+
+    if(count($errors) > 0){
+        echo '<div class="error">';
+        foreach ($errors as $error){
+            echo $error .'<br>';
+        }
+        echo '</div>';
+    }
+}
+
+
+?>
---
+++ GFHLAAAA/multi_login/index.php
@@ -0,0 +1,48 @@
+<?php 
+    include('functions.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="header">
+    <h2>Home Page</h2>
+    <div>
+    <div class="content">
+
+    <?php if (isset($_SESSION['success'])) : ?>
+    <div class="error success">
+    <h3>
+        <?php 
+            echo $_SESSION['success'];
+            unset($_SESSION['success']);
+        ?>
+    </h3>
+</div>
+<?php endif ?>
+
+<div class="profile_info">
+			<img src="img/group2.png"  >
+
+			<div>
+				<?php  if (isset($_SESSION['user'])) : ?>
+					<strong><?php echo $_SESSION['user']['username']; ?></strong>
+
+					<small>
+						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
+						<br>
+						<a href="index.php?logout='1'" style="color: red;">logout</a>
+					</small>
+
+				<?php endif ?>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
