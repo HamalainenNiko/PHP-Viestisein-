a297d394c60d022bd29e9be955fc733eacc54830
Niko Hämäläinen <niko.hamalainen@itvelhot.local>
2019-09-25 11:50:35 +0300

    new profile layout

--- GFHLAAAA/multi_login/profile.php
+++ GFHLAAAA/multi_login/profile.php
@@ -4,6 +4,10 @@
         $_SESSION['msg'] = "You must login first";
         header('location: login.php');
     }
+
+    if(isAdmin()){
+        header('location: admin/home.php');
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,50 +16,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="main.css">
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 <header>
     <section id="link">   
         <nav>
-                <li><a href="../index.html" id="left">G.F.H.L.A.A.A.A</a></li>
-
-
-              </nav>
+            <a href="../index.php" id="left">G.F.H.L.A.A.A.A</a>
+        </nav>
+        <div class="content">
+            <div class="profile_info">
+            <img src="img/group2.png">
+        <?php echo $_SESSION['user']['username']; ?>
+        <i style="color #888;"><?php echo ucfirst($_SESSION['user']['user_type']); ?>
+        <br>
+        <a href="profile.php?logout='1'" style="color: red;">logout</a>
+    </div>
     </section>    
 </header>
-    <div class="header">
+
+<div class="header">
     <h2>Home Page</h2>
-    </div>
-    <div class="content">
+</div>
 
     <?php if (isset($_SESSION['success'])) : ?>
     <div class="error success">
-    <h3>
+        <h3>
         <?php 
             echo $_SESSION['success'];
             unset($_SESSION['success']);
         ?>
-    </h3>
+        </h3>
+    </div>
+    <?php endif ?>
+
+
+    <div class="card">
+  <img src="img/group2.png"  style="width:100%">
+  <h1><?php echo $_SESSION['user']['username']; ?></h1>
+  <p class="title"> <i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i></p>
+  <p>G.F.H.L.A.A.A.A</p>
+
+  <p><button></button></p>
 </div>
-<?php endif ?>
 
-		<div class="profile_info">
-			<img src="img/group2.png"  >
 
-			<div>
-				<?php  if (isset($_SESSION['user'])) : ?>
-					<strong><?php echo $_SESSION['user']['username']; ?></strong>
-
-					<small>
-						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
-						<br>
-						<a href="profile.php?logout='1'" style="color: red;">logout</a>
-					</small>
-
-				<?php endif ?>
-			</div>
-		</div>
-	</div>
 </body>
 </html>
