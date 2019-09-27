108bcec03c4994a798c0550e41f8bdc6c02c1375
Niko Hämäläinen <niko.hamalainen@itvelhot.local>
2019-09-27 13:16:09 +0300

    dsa

--- GFHLAAAA/multi_login/profile.php
+++ GFHLAAAA/multi_login/profile.php
@@ -19,6 +19,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="main.css">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script2.js"></script>
 </head>
 <body>
 <header>
@@ -26,20 +27,18 @@
         <nav>
             <a href="../index.php" id="left">G.F.H.L.A.A.A.A</a>
         </nav>
-        <div class="profile_info">
+        <div class="profile_info" id="myForm">
             <img src="img/group2.png">
         <?php echo $_SESSION['user']['username']; ?>
-        <i style="color #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)
+        <i  style="color #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)
         <br>
         <a href="profile.php?logout='1'" style="color: red;">logout</a></i>
     </div>
     </section>    
 </header>
-
 <div class="header">
     <h2>Home Page</h2>
 </div>
-
     <?php if (isset($_SESSION['success'])) : ?>
     <div class="error success">
         <h3>
@@ -50,16 +49,12 @@
         </h3>
     </div>
     <?php endif ?>
-
-
     <div class="card">
   <img src="img/group2.png"  style="width:100%">
   <h1><?php echo $_SESSION['user']['username']; ?></h1>
   <p class="title"> <i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i></p>
   <p>G.F.H.L.A.A.A.A</p>
-
   <p><button></button></p>
 </div>
-
 </body>
 </html>
