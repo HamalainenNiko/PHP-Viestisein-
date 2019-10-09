3945b493f5e6563381a3912e7b1c05dd9865c71a
Niko Hämäläinen <niko.hamalainen@itvelhot.local>
2019-10-09 12:33:27 +0300

    jugfjhgfjhgfkh

--- GFHLAAAA/multi_login/info.php
+++ GFHLAAAA/multi_login/info.php
@@ -12,18 +12,18 @@
     <title>Document</title>
 </head>
 <body>
-<header>
-<section id="link">   
-        <nav>
-                <li><a href="index.php" id="left">G.F.H.L.A.A.A.A</a></li>
-<?php if(!isset($_SESSION['user'])) : ?>
-<li><a href="login.php" id="right">Login</a></li>
-<?php endif ?>
-      <?php  if (isset($_SESSION['user'])) : ?>
-      <li><a href="profile.php">Profile</a></li>
+  <header>
+    <section id="link">   
+      <nav>
+        <li><a href="index.php" id="left">G.F.H.L.A.A.A.A</a></li>
+        <?php if(!isset($_SESSION['user'])) : ?>
+        <li><a href="login.php" id="right">Login</a></li>
+        <?php endif ?>
+        <?php  if (isset($_SESSION['user'])) : ?>
+        <li><a href="profile.php">Profile</a></li>
       </nav>
       <div class="profile_info">
-		<a href="profile.php"> <img src=<?php echo 'images/'. $_SESSION['user']['profile_image']; ?>></a>
+		    <a href="profile.php"> <img src=<?php echo 'images/'. $_SESSION['user']['profile_image']; ?>></a>
 				<strong>
           <?php echo $_SESSION['user']['username']; ?>
         </strong>
@@ -35,25 +35,24 @@
 				<?php endif ?>
 			</div>
     </section>    
-</header>
-<br>
-<div  class="card">
-<?php 
-    $id = $_GET['id'];
-    $id = mysqli_real_escape_string($db,$id);
-    $sql = "SELECT * FROM users WHERE id='" . $id . "'";
-    $result = mysqli_query($db, $sql);
-    
-    while($row = mysqli_fetch_array($result)){
-      echo "<img src='images/" .$row['profile_image']."' />";
-      echo '<h1>'.$row['username'].'</h1>';
-      echo '<p class=title><i  style=color: #888;>('.$row['user_type'].') <br></p>';
-      echo '<p>Bio: <br>'.$row['info'].'</p>';
-      echo '<p>G.F.H.L.A.A.A.A</p>';
-      echo '<p><button></button></p>';
-        
-    }
-    ?>
+  </header>
+  <br>
+  <div  class="card">
+    <?php 
+      $id = $_GET['id'];
+      $id = mysqli_real_escape_string($db,$id);
+      $sql = "SELECT * FROM users WHERE id='" . $id . "'";
+      $result = mysqli_query($db, $sql);
+
+      while($row = mysqli_fetch_array($result)){
+        echo "<img src='images/" .$row['profile_image']."' />";
+        echo '<h1>'.$row['username'].'</h1>';
+        echo '<p class=title><i  style=color: #888;>('.$row['user_type'].') <br></p>';
+        echo '<p>Bio: <br>'.$row['info'].'</p>';
+        echo '<p>G.F.H.L.A.A.A.A</p>';
+        echo '<p><button></button></p>';
+      }
+      ?>
   </div>
 </body>
 </html>
