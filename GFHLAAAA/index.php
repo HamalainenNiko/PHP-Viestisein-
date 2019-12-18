9c7af08eb4fd54a624d403aae6d723ac6cf0f05c
Niko Hämäläinen <niko.hamalainen@itvelhot.local>
2019-12-18 09:33:13 +0200

    loremipsumipsumlorem

--- GFHLAAAA/index.php
+++ GFHLAAAA/index.php
@@ -40,8 +40,8 @@
     </section>    
   </header>
   <div class="content">
-    <h2>This is not funny guys.</h2>
-    <p> Not funny didn't laugh.<br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quisquam, 
+    <h2>Lorem Ipsum.</h2>
+    <p> Ipsum Lorem.<br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quisquam, 
     accusamus culpa impedit rem atque est facere <br> magni ducimus esse laborum ea? Iure esse excepturi debitis facere reprehenderit! Totam ipsa ven
     rem necessitatibus maiores <br> consequatur. Doloremque reiciendis veritatis quod? Harum.
     <ul>Existing members:
